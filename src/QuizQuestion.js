0c6a7065a979631194611959d1dd76746cd08a8d
Anastasiia Onchukova <aonchukova@guidewire.com>
2018-08-06 10:17:44 +0200

    Module 6

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -1,28 +1,42 @@
 import React, {Component} from 'react'
 import QuizQuestionButton from './QuizQuestionButton'
 
-class QuizQuestion extends Component{
-    constructor(props){
+class QuizQuestion extends Component {
+    constructor(props) {
         super(props)
+        this.state = {
+            incorrectAnswer: false
+        }
     }
-    render(){
-        return(
+
+    render() {
+        return (
             <main>
                 <section>
                     <p>{this.props.quiz_question.instruction_text}</p>
                 </section>
                 <section className="buttons">
                     <ul>
-                        {this.props.quiz_question.answer_options.map((answer_option,index)=>
-                            <QuizQuestionButton clickHandler={this.handleClick.bind(this)} button_text={answer_option} key={index}/>)}
+                        {this.props.quiz_question.answer_options.map((answer_option, index) =>
+                            <QuizQuestionButton clickHandler={this.handleClick.bind(this)} button_text={answer_option}
+                                                key={index}/>)}
                     </ul>
                 </section>
+                {this.state.incorrectAnswer ? (<p className='error'>Sorry, that's not right</p>) : null}
             </main>
         )
     }
 
-    handleClick(buttonText){
-            if(buttonText===this.props.quiz_question.answer){this.props.showNextQuestionHandler();}
+    handleClick(buttonText) {
+        if (buttonText === this.props.quiz_question.answer) {
+            this.setState({incorrectAnswer: false});
+            this.props.showNextQuestionHandler();
+        }
+        else {
+            this.setState({
+                incorrectAnswer: true
+            });
+        }
     }
 }
 
